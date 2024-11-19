ec3088a3656c32467587626f74c82a28d8ed1ce5
tomcat8 <tomcat8@svr-acr31-chime.cl.cam.ac.uk>
2024-11-19 21:10:38 +0000

    Initialised next step

---
+++ src/test/java/uk/ac/cam/jrs237/fibonacci/FibonacciTableTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2024 Andrew Rice <acr31@cam.ac.uk>, J.R. Shovelton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cam.jrs237.fibonacci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FibonacciTableTest {
+
+  @Test
+  public void fib_makesUseOfCache() {
+    // Hint: use CountingMap!
+  }
+}
