d7036944246e0cf24fe9f63675b2504f47a52623
tomcat8 <tomcat8@svr-acr31-chime.cl.cam.ac.uk>
2024-11-19 20:39:29 +0000

    Initialised task

---
+++ src/main/java/uk/ac/cam/jrs237/fibonacci/Fibonacci.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 Andrew Rice <acr31@cam.ac.uk>, J.R. Shovelton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cam.jrs237.fibonacci;
+
+/** A class to compute Fibonacci numbers using a simple recursive approach. */
+class Fibonacci {
+
+  /**
+   * Compute a Fibonacci number.
+   *
+   * @param i the index in the Fibonacci sequence
+   * @return the Fibonacci number for this index
+   */
+  int fib(int i) {
+    throw new UnsupportedOperationException();
+  }
+}
